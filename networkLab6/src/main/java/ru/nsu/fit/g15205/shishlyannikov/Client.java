1c7804a1ff78c887e8a6007481ae5e5be930c0e6
byDimasik <d.shishliannikov@g.nsu.ru>
2017-11-26 00:23:25 +0700

    клиент умеет логиниться

--- networkLab6/src/main/java/ru/nsu/fit/g15205/shishlyannikov/Client.java
+++ networkLab6/src/main/java/ru/nsu/fit/g15205/shishlyannikov/Client.java
@@ -1,8 +1,13 @@
 package ru.nsu.fit.g15205.shishlyannikov;
 
+import com.google.gson.Gson;
+import ru.nsu.fit.g15205.shishlyannikov.utils.HttpHeaderBuilder;
+
 import java.io.*;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Client {
     public static void main(String[] args) throws InterruptedException {
@@ -13,33 +18,47 @@ public class Client {
              DataInputStream in = new DataInputStream(socket.getInputStream()) ) {
 
             socket.setSoTimeout(500);
-            String clientCommand = "POST /login HTTP/1.1\r\n" +
-                                   "Content-Type: application/json\r\n";
+            HttpHeaderBuilder headerBuilder = new HttpHeaderBuilder();
+            Gson gson = new Gson();
 
-            while (true) {
-                if (br.ready()) {
-                    clientCommand = br.readLine();
-                    if (clientCommand != null) {
-                        if (clientCommand.equalsIgnoreCase("exit")) {
-                            break;
-                        }
+            System.out.print("Введите имя: ");
+            String username = br.readLine();
 
-                        out.writeUTF(clientCommand);
-                    }
-                }
+            Map<String, String> loginMap = new HashMap<>();
+            loginMap.put("username", username);
+            String loginJson = gson.toJson(loginMap, HashMap.class);
 
-                try {
-                    String recv = in.readUTF();
-                    if (recv.equalsIgnoreCase("END")) {
-                        break;
-                    }
-                    System.out.println(recv);
-                } catch (SocketTimeoutException ignored) {
-                } catch (IOException ex) {
-                    break;
-                }
+            String loginHeader = headerBuilder.buildRequestLogin(loginJson.length());
+            String requestLogin = loginHeader + loginJson;
 
-            }
+
+            out.write(requestLogin.getBytes());
+            out.flush();
+
+//            while (true) {
+//                if (br.ready()) {
+//                    clientCommand = br.readLine();
+//                    if (clientCommand != null) {
+//                        if (clientCommand.equalsIgnoreCase("exit")) {
+//                            break;
+//                        }
+//
+//                        out.writeUTF(clientCommand);
+//                    }
+//                }
+//
+//                try {
+//                    String recv = in.readUTF();
+//                    if (recv.equalsIgnoreCase("END")) {
+//                        break;
+//                    }
+//                    System.out.println(recv);
+//                } catch (SocketTimeoutException ignored) {
+//                } catch (IOException ex) {
+//                    break;
+//                }
+//
+//            }
         } catch (IOException e) {
             e.printStackTrace();
         }
--- networkLab6/src/main/java/ru/nsu/fit/g15205/shishlyannikov/utils/HttpHeaderBuilder.java
+++ networkLab6/src/main/java/ru/nsu/fit/g15205/shishlyannikov/utils/HttpHeaderBuilder.java
@@ -10,30 +10,39 @@ public class HttpHeaderBuilder {
     private final String responseTypeMethodNotAllowed = "HTTP 405 Method Not Allowed";
     private final String responseTypeInternalServerError = "HTTP 500 Internal Server Error";
 
-    private final String responseHeaderLength = "Content-Length: ";
-    private final String responseHeaderContent = "Content-Type: application/json";
-    private final String responseHeaderToken = "Authorization: Token ";
-    private final String responseHeaderWWWAuthenticate = "WWW-Authenticate: ";
+    private final String requestTypeLogin = "POST /login HTTP/1.1";
+
+
+    private final String headerLength = "Content-Length: ";
+    private final String headerContent = "Content-Type: application/json";
+    private final String headerToken = "Authorization: Token ";
+    private final String headerWWWAuthenticate = "WWW-Authenticate: ";
 
     public String buildResponseOK(int contentLength) {
         return responseTypeOK + endRow +
-               responseHeaderContent + endRow +
-               responseHeaderLength + contentLength + endRow + endRow;
+                headerContent + endRow +
+                headerLength + contentLength + endRow + endRow;
     }
 
     public String buildResponseUnauthorized(String message) {
         return responseTypeUnauthorized + endRow +
-               responseHeaderLength + "0" + endRow +
-               responseHeaderWWWAuthenticate + message + endRow + endRow;
+                headerLength + "0" + endRow +
+                headerWWWAuthenticate + message + endRow + endRow;
     }
 
     public String buildResponseForbidden() {
         return responseTypeForbidden + endRow +
-               responseHeaderLength + "0" + endRow + endRow;
+                headerLength + "0" + endRow + endRow;
     }
 
     public String buildResponseNotFound() {
         return responseTypeNotFound + endRow +
-               responseHeaderLength + "0" + endRow + endRow;
+                headerLength + "0" + endRow + endRow;
+    }
+
+    public String buildRequestLogin(int contentLength) {
+        return requestTypeLogin + endRow +
+               headerContent + endRow +
+               headerLength + contentLength + endRow + endRow;
     }
 }
